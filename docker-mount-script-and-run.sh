c39b1d2a4eebbce919d9bd032869825f948b2f16
Siddharth Bhat <siddu.druid@gmail.com>
2026-06-25 12:00:21 +0100

    chore: change docker container name

--- docker-mount-script-and-run.sh
+++ docker-mount-script-and-run.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 # Shell script to mount the scripts directory and run the results.
 
-podman run --mount type=bind,src="$(pwd)/runresults",dst=/workspace/runresults  -it localhost/practical-misplace-monarch "$@"
+podman run --mount type=bind,src="$(pwd)/runresults",dst=/workspace/runresults  -it localhost/fp-lean-eval "$@"
 
